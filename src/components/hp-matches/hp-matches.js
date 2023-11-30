33093d28953d9182cf0fc07f3feba9e212ff1206
mizza-cz <mizza.cz666@gmail.com>
2023-11-30 11:26:50 +0100

    add game

--- src/components/hp-matches/hp-matches.js
+++ src/components/hp-matches/hp-matches.js
@@ -1,10 +1,23 @@
-$(".hp-matches .menuTabs .menuTab").on("click", function (event) {
-  var id = $(this).attr("data-id");
-  $(".hp-matches .menuTab__content").removeClass("tab-active").hide();
-  $(".hp-matches .menuTabs .menuTab").removeClass("active");
-  $(this).addClass("active");
-  $("#" + id)
-    .addClass("tab-active")
-    .fadeIn();
-  return false;
-});
+// $(".hp-matches .menuTabs .menuTab").on("click", function (event) {
+//   var id = $(this).attr("data-id");
+//   $(".hp-matches .menuTab__content").removeClass("tab-active").hide();
+//   $(".hp-matches .menuTabs .menuTab").removeClass("active");
+//   $(this).addClass("active");
+//   $("#" + id)
+//     .addClass("tab-active")
+//     .fadeIn();
+//   return false;
+// });
+
+
+
+$(function () {
+  var hash = window.location.hash;
+  hash && $('ul.nav a[href="' + hash + '"]').tab("show");
+
+  $("body").on("click", ".nav-tabs a", function (e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    $(this).tab("show");
+  });
+}),
